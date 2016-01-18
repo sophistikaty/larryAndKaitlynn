b7e102f867b6df1215d7de7a11ee9272ea7a6d5d
sophistikaty <sophistikaty@me.com>
2016-01-18 09:46:15 -0500

    more content, re-org

--- js/main.js
+++ js/main.js
@@ -1,58 +1,21 @@
 
 $(document).ready(function(){
 
-  // 1. Write a function that loops through numbers 1 - 100;
+	'use strict';
 
-	 function hundred ()	{
-	 	var i;
-		for (i=0; i<=100; i++) {
-		console.log (i);
-		document.write(i + ' ');
-		}
-	 }
+  console.log('ready');
 
- hundred ();
+  let moreButtons = document.querySelectorAll('button.more');
 
+  console.log('moreButtons is ', moreButtons);
 
-  //    If the number is divisible by 3, print 'fizz'
+	for ( let b = 0 ; b < moreButtons.length ; b++ ){
 
-   function hundred3fizz ()	{
-	 	var k;
-		for (k=1; k<=100; k++) {
-			if (k%3==0) {
-				document.write(k + ' fizz ');
-			}	
-		}
-	 }
+		console.log('moreButtons[b] is', moreButtons[b].dataset.section); 
 
- hundred3fizz ();
+		let currentSection = moreButtons[b].dataset.section; 
 
-  //    If the number is divisible by 5, print 'buzz'
-
-    function hundred5buzz ()	{
-	 	var l;
-		for (l=1; l<=100; l++) {
-			if (l%5==0) {
-				document.write(l + ' buzz ');
-			}	
-		}
-	 }
-
- hundred5buzz ();
-
-  //    If the number is divisible by 3 AND 5 print 'fizz buzz';
-
-     function hundred35fizzbuzz ()	{
-	 	var m;
-		for (m=1; m<=100; m++) {
-			if (m%5==0 && m%3==0) {
-				document.write(m + ' fizzbuzz ');
-			}	
-		}
-	 }
-
- hundred35fizzbuzz ();
-  //
-
+		console.log('currentSection is ',currentSection);
+	}
 
 });
